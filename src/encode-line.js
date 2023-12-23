b386296c92e32a0233beb26dc6426db6d50ff9fe
YermakA <ermak.080@mail.ru>
2023-12-23 22:49:49 +0800

    feat: some tasks complete

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let arr = [];
+  let k = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1]) {
+      if (k !== 1) {
+        arr.push(k)
+      }
+      arr.push(str[i]);
+      k = 1;
+    } else {
+      k += 1;
+    }
+
+  }
+
+  return arr.join('');
+
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let string = String(str);
+  let result = '';
+
+  for (let i = 0; i < options.repeatTimes || i <= 0; i++) {
+    result += string;
+
+    if (String(options.addition) && options.addition !== undefined) {
+      for (let j = 0; j < options.additionRepeatTimes || j <= 0; j++) {
+        result += String(options.addition);
+        if (options.additionSeparator && (j < options.additionRepeatTimes - 1)) {
+          result += String(options.additionSeparator);
+        } else if (j < options.additionRepeatTimes - 1) {
+          result += '|'
+        }
+      }
+    }
+
+    if (options.separator && i < options.repeatTimes - 1) {
+      result += String(options.separator);
+    } else if (i < options.repeatTimes - 1) {
+      result += '+';
+    }
+
+  }
+
+  return result;
 }
 
 module.exports = {
